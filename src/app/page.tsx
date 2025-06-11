f7efc50d5f974d3f9b2e3557b838f812c052a2c8
Arlindo Almada <1arlindoalmada@gmail.com>
2025-06-11 08:56:04 +0100

    Adicionar imagens dos produtos no e-commerce com as novas imagens PNG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -456,7 +456,7 @@ export default function Home() {
         {/* Footer */}
         <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t glassmorphism">
           <p className="text-xs text-gray-500 dark:text-gray-400">
-            © 2024 Sonangol. Privacidade Garantida - IA Ética - Desenvolvido por
+            © 2025 Sonangol. Privacidade Garantida - IA Ética - Desenvolvido por
             Arlindo Almada
           </p>
           <nav className="sm:ml-auto flex gap-4 sm:gap-6">
